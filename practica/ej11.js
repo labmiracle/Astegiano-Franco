e8c7c5e83a42b8d8c9c4d45ef2f7466a898dbd72
Franco <f.astegiano@gmail.com>
2023-04-23 18:37:38 -0300

    ej11.rev

--- practica/ej11.js
+++ practica/ej11.js
@@ -1,10 +1,36 @@
 function addOnlyUNiq() {
 
-    let uniq = new Set(arguments);
-    let res = Array.from(uniq);
+    let res = ""
+    for(let i of arguments){
+        if(!repeats(arguments, i)){
+            res += i
+        }
+
+    }
+
     return res;    
 
 }
 
 
-console.log(addOnlyUNiq(1,1,6,"Hola","dd",6,"hola"));
+console.log(addOnlyUNiq(1,1,6,"Hola","dd",6,"hola",8));
+
+
+function repeats(arr, val){
+
+    let count = 0;
+
+    for(let i of arr){
+       if(i === val){
+        count++
+       }
+    }
+
+    if (count>1){
+        return true;
+    }
+
+    return false;
+
+
+}
